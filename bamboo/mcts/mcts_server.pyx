900dcb13adf27ea3d243fc27c0dd971fba42c21e
hiropppe <sergei.0910@gmail.com>
2017-08-02 09:25:21 +0900

    Fix bug.

--- bamboo/mcts/mcts_server.pyx
+++ bamboo/mcts/mcts_server.pyx
@@ -9,7 +9,7 @@ from libc.stdio cimport printf
 from bamboo.go.board cimport S_BLACK 
 from bamboo.go.board cimport FLIP_COLOR 
 from bamboo.go.board cimport game_state_t
-from bamboo.go.board cimport set_board_size, set_komi, allocate_game, initialize_board, put_stone
+from bamboo.go.board cimport set_board_size, set_komi, komi, allocate_game, initialize_board, put_stone
 from bamboo.go.printer cimport print_board
 from bamboo.rollout.preprocess cimport initialize_rollout, update_rollout
 from bamboo.mcts.tree_search cimport MCTS
@@ -21,7 +21,7 @@ cdef class MCTSServer(object):
     def __cinit__(self,
                   object policy,
                   double temperature=0.67,
-                  int playout_limit=5000,
+                  int playout_limit=8000,
                   int n_threads=1):
         self.mcts = MCTS(policy, temperature, playout_limit, n_threads)
 
@@ -66,8 +66,8 @@ cdef class MCTSServer(object):
     def set_size(self, bsize):
         set_board_size(bsize)
 
-    def set_komi(self, komi):
-        set_komi(komi)
+    def set_komi(self, new_komi):
+        set_komi(new_komi)
 
     def set_time(self, m, b, stone):
         pass
@@ -81,8 +81,9 @@ cdef class MCTSServer(object):
     def save_sgf(self, black_name, white_name):
         from tempfile import NamedTemporaryFile
         temp_file = NamedTemporaryFile(delete=False)
-        save_gamestate_to_sgf(self.game, '/tmp/', temp_file, black_name, white_name)
-        return temp_file.name
+        temp_file_name = temp_file.name + '.sgf'
+        save_gamestate_to_sgf(self.game, '/tmp/', temp_file_name, black_name, white_name)
+        return temp_file_name
 
     def quit(self):
         self.mcts.stop_search_thread()
