11cd4837195b88096c54a30ea2e3762c59908989
hiropppe <sergei.0910@gmail.com>
2017-10-05 07:47:46 +0000

    Testing value net.

--- bamboo/models/dcnn_value.py
+++ bamboo/models/dcnn_value.py
@@ -13,18 +13,6 @@ FILTER_WIDTH_1 = 5
 FILTER_WIDTH_2_12 = 3
 
 
-def get_states():
-    return tf.placeholder(
-                tf.float32,
-                shape=(None, BOARD_SIZE, BOARD_SIZE, INPUT_DEPTH))
-
-
-def get_actions():
-    return tf.placeholder(
-                tf.float32,
-                shape=(None, BOARD_SIZE**2))
-
-
 def get_initial_weight(layer, wb, scope_name):
     if wb.lower() == 'w':
         if layer == 1:
@@ -32,34 +20,34 @@ def get_initial_weight(layer, wb, scope_name):
                 scope_name + '_W',
                 [FILTER_WIDTH_1, FILTER_WIDTH_1, INPUT_DEPTH, FILTER_SIZE],
                 minval=-0.05, maxval=0.05)
-        elif layer <= 13:
+        elif layer <= 12:
             return nn_util.random_uniform(
                 scope_name + '_W',
                 [FILTER_WIDTH_2_12, FILTER_WIDTH_2_12, FILTER_SIZE, FILTER_SIZE],
                 minval=-0.05, maxval=0.05)
-        elif layer == 14:
+        elif layer == 13:
             return nn_util.random_uniform(
                 scope_name + '_W',
                 [1, 1, FILTER_SIZE, 1],
                 minval=-0.05, maxval=0.05)
-        elif layer == 15:
+        elif layer == 14:
             return nn_util.random_uniform(
                 scope_name + '_W',
                 [361, 256],
                 minval=-0.05, maxval=0.05)
-        elif layer == 16:
+        elif layer == 15:
             return nn_util.random_uniform(
                 scope_name + '_W',
                 [256, 1],
                 minval=-0.05, maxval=0.05)
     elif wb.lower() == 'b':
-        if 1 <= layer and layer <= 13:
+        if 1 <= layer and layer <= 12:
             return nn_util.zero_variable(scope_name + '_b', [FILTER_SIZE])
-        elif layer == 14:
+        elif layer == 13:
             return nn_util.zero_variable(scope_name + '_b', [1])
-        elif layer == 15:
+        elif layer == 14:
             return nn_util.zero_variable(scope_name + '_b', [256])
-        elif layer == 16:
+        elif layer == 15:
             return nn_util.zero_variable(scope_name + '_b', [1])
 
 
@@ -72,9 +60,9 @@ def inference(states):
         bias_add = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(bias_add, name=scope.name)
 
-    # convolution2d_2-13
+    # convolution2d_2-12
     convi = conv1
-    for i in range(2, 14):
+    for i in range(2, 13):
         with tf.variable_scope('convolution2d_' + str(i)) as scope:
             weights = get_initial_weight(i, 'w', scope.name)
             biases = get_initial_weight(i, 'b', scope.name)
@@ -83,30 +71,30 @@ def inference(states):
             conv = tf.nn.relu(bias_add, name=scope.name)
         convi = conv
 
-    # convolution2d_14
-    with tf.variable_scope('convolution2d_14') as scope:
-        weights = get_initial_weight(14, 'w', scope.name)
-        biases = get_initial_weight(14, 'b', scope.name)
+    # convolution2d_13
+    with tf.variable_scope('convolution2d_13') as scope:
+        weights = get_initial_weight(13, 'w', scope.name)
+        biases = get_initial_weight(13, 'b', scope.name)
         conv = tf.nn.conv2d(convi, weights, [1, 1, 1, 1], padding='SAME')
         bias_add = tf.nn.bias_add(conv, biases)
-        layer_14 = tf.nn.relu(bias_add, name=scope.name)
+        layer_13 = tf.nn.relu(bias_add, name=scope.name)
 
-    # Dense 15
-    with tf.variable_scope('dense_15') as scope:
-        flatten = tf.reshape(layer_14, [-1, BOARD_SIZE**2])
-        weights = get_initial_weight(15, 'w', scope.name)
-        biases = get_initial_weight(15, 'b', scope.name)
-        layer_15 = tf.nn.relu(tf.matmul(flatten, weights) + biases)
+    # Dense 14
+    with tf.variable_scope('dense_14') as scope:
+        flatten = tf.reshape(layer_13, [-1, BOARD_SIZE**2])
+        weights = get_initial_weight(14, 'w', scope.name)
+        biases = get_initial_weight(14, 'b', scope.name)
+        layer_14 = tf.nn.relu(tf.matmul(flatten, weights) + biases)
 
     # output
     with tf.variable_scope('tanh') as scope:
-        weights = get_initial_weight(16, 'w', scope.name)
-        biases = get_initial_weight(16, 'b', scope.name)
-        output = tf.nn.tanh(tf.matmul(layer_15, weights) + biases)
+        weights = get_initial_weight(15, 'w', scope.name)
+        biases = get_initial_weight(15, 'b', scope.name)
+        output = tf.nn.tanh(tf.matmul(layer_14, weights) + biases)
 
     return output
 
 
 def loss(output, z):
-    loss_op = tf.losses.mean_squared_error(output, z)
+    loss_op = tf.losses.mean_squared_error(z, output)
     return loss_op
