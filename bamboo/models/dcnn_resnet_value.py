1643b4fde331af8be89c2605ff77e55556831156
hiropppe <sergei.0910@gmail.com>
2017-10-11 00:54:41 +0000

    Update value resnet.

--- bamboo/models/dcnn_resnet_value.py
+++ bamboo/models/dcnn_resnet_value.py
@@ -15,23 +15,31 @@ def inference(states, data_format='channels_last', is_training=False):
 
     with tf.variable_scope('block_layer1') as scope:
         inputs = block_layer(
-            inputs=inputs, filters=64, block_fn=building_block, blocks=6,
+            inputs=inputs, filters=64, block_fn=building_block, blocks=2,
             strides=1, is_training=is_training, name=scope.name,
+            data_format=data_format)
+
+    with tf.variable_scope('block_layer2') as scope:
+        inputs = block_layer(
+            inputs=inputs, filters=128, block_fn=building_block, blocks=2,
+            strides=2, is_training=is_training, name=scope.name,
+            data_format=data_format)
+
+    with tf.variable_scope('block_layer3') as scope:
+        inputs = block_layer(
+            inputs=inputs, filters=256, block_fn=building_block, blocks=2,
+            strides=2, is_training=is_training, name=scope.name,
             data_format=data_format)
 
     with tf.variable_scope('final_conv'):
         inputs = batch_norm_relu(inputs, is_training, data_format)
-        inputs = conv2d_fixed_padding(
-            inputs=inputs, filters=1, kernel_size=1, strides=1,
+        inputs = tf.layers.average_pooling2d(
+            inputs=inputs, pool_size=5, strides=1, padding='VALID',
             data_format=data_format)
         inputs = tf.identity(inputs, scope.name)
 
-    with tf.variable_scope('dense_256'):
-        inputs = tf.reshape(inputs, [-1, inputs.shape[2].value**2])
-        inputs = tf.layers.dense(inputs=inputs, units=256, activation=tf.nn.relu)
-        inputs = tf.identity(inputs, scope.name)
-
     with tf.variable_scope('final_activation'):
+        inputs = tf.reshape(inputs, [-1, 256])
         inputs = tf.layers.dense(inputs=inputs, units=1)
         inputs = tf.nn.tanh(inputs)
         inputs = tf.identity(inputs, scope.name)
