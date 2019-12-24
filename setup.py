0607424fe05341e448aedd0b4db052d76599258f
hiropppe <sergei.0910@gmail.com>
2019-12-24 19:09:49 +0000

    suppress warning

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 
+# Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
+import distutils.sysconfig
+cfg_vars = distutils.sysconfig.get_config_vars()
+for key, value in cfg_vars.items():
+    if type(value) == str:
+        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+# ==================================
 
 extensions = [Extension('bamboo.board', sources=['bamboo/board.pyx'], language="c++", extra_compile_args=["-std=c++11"]),
               Extension('bamboo.zobrist_hash', sources=["bamboo/zobrist_hash.pyx"], language="c++", extra_compile_args=["-std=c++11"]),
